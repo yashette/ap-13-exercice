bd49949b2bee2a056f26833e472ec9a26bda08f6
yashette <79081473+yashette@users.noreply.github.com>
2024-03-04 12:14:18 +0100

    first commit

---
+++ node_modules/eslint-config-kentcdodds/es6/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    './non-rules-config.js',
+    './deprecated-rules.js',
+    './stylistic.js',
+    './best-practices.js',
+    './possible-errors.js',
+  ],
+  rules: {},
+}
---
+++ node_modules/eslint-config-kentcdodds/import/best-practices.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: './non-rules-config.js',
+  rules: {
+    'import/first': 'error',
+    'import/no-anonymous-default-export': 'off',
+    'import/no-absolute-path': 'error',
+    'import/no-deprecated': 'warn', // this is an in progress rule
+    'import/no-duplicates': 'error',
+    'global-require': 'off', // disable because no need to have both!
+    'import/no-internal-modules': 'off',
+    'import/no-mutable-exports': 'error',
+    'import/no-restricted-paths': 'off',
+    'import/no-unassigned-import': 'off',
+    'import/no-webpack-loader-syntax': 'error',
+    'import/no-unused-modules': 'off',
+  },
+}

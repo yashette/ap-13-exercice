a9823568d44182f32c98938a8bb1471ac698b660
yashette <79081473+yashette@users.noreply.github.com>
2024-03-04 13:47:01 +0100

    Update server.js

--- tests/server.js
+++ tests/server.js
@@ -5,16 +5,23 @@ export const handlers = [
   rest.post('/greeting', (req, res, ctx) =>
     res(ctx.json({data: {greeting: `Hello ${req.body.subject}`}})),
   ),
-  rest.post('/form', (req, res, ctx) =>
-    res(ctx.status(200),
-      ctx.json({data: {greeting: `${req.body.subject}`}})),
-  ),
   rest.post('/post/:id', (req, res, ctx) => {
     if (!req.body.title) {
       return res(
         ctx.status(400),
         ctx.json({
           errorMessage: 'Format invalide, veuillez renseigner le titre',
+        }),
+      )
+    }
+    return res(ctx.json({data: req.body}))
+  }),
+  rest.post('/form', (req, res, ctx) => {
+    if (!req.body.drink || !req.body.food) {
+      return res(
+        ctx.status(400),
+        ctx.json({
+          message: 'les champs food et drink sont obligatoires',
         }),
       )
     }
